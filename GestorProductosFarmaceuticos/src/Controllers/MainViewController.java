c46eff66d3ce96c9ece06395682c894c5906596f
Santino Casado <santinocasado05@gmail.com>
2025-08-13 14:22:05 -0300

    add

--- GestorProductosFarmaceuticos/src/Controllers/MainViewController.java
+++ GestorProductosFarmaceuticos/src/Controllers/MainViewController.java
@@ -1,19 +1,35 @@
 package Controllers;
 
+import Enums.TipoVehiculos;
+import Exceptions.PatenteRepetidaException;
+import Gestor.AdministradorVehiculos;
+import Models.Vehiculo;
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import jdk.jfr.Label;
 
 public class MainViewController implements Initializable {
+    @FXML
+    private ListView<Vehiculo> ListViewVehiculos;
+    
+    private ArrayList<Vehiculo> listaVehiculos;
+    private AdministradorVehiculos administrador;
 
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-       
-    }
     //Botones
     @FXML
     private Button btnAgregar;
@@ -28,9 +44,9 @@ public class MainViewController implements Initializable {
     
     //Choice Box
     @FXML
-    private ChoiceBox<?> cbFiltrarEstado;
+    private ChoiceBox<String> cbFiltrarEstado;
     @FXML
-    private ChoiceBox<?> cbFiltrarTipo;
+    private ChoiceBox<TipoVehiculos> cbFiltrarTipo;
     
     //Label
     @FXML
@@ -39,4 +55,93 @@ public class MainViewController implements Initializable {
     private Label lblSegundoAtributo;
     
     
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Inicializo los Choice Boxes
+        this.cbFiltrarEstado.getItems().addAll("Disponible", "En Uso", "En Mantenimiento", "TODOS");
+        this.cbFiltrarEstado.setValue("TODOS");
+        
+        this.cbFiltrarTipo.getItems().addAll(TipoVehiculos.AUTO, TipoVehiculos.CAMIONETA, TipoVehiculos.MOTO, TipoVehiculos.TODOS);
+        this.cbFiltrarTipo.setValue(TipoVehiculos.TODOS);
+        
+       administrador = new AdministradorVehiculos();
+       
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //CRUD
+    @FXML
+    void agregar(ActionEvent event) {
+        this.AbrirFormulario(null);
+    }
+    
+    @FXML
+    void eliminar(ActionEvent event) {
+         //Chequeo que se haya seleccionado algo en mi listView
+        Vehiculo pf = this.ListViewVehiculos.getSelectionModel().getSelectedItem();
+        
+        if (pf != null) {
+            //Muestro una alerta de confirmacion
+            Alert alerta = new Alert(Alert.AlertType.CONFIRMATION);
+            alerta.setTitle("Confirmar eliminacion");
+            alerta.setHeaderText("Estas seguro de eliminar estos datos?");
+            alerta.setContentText(pf.toString());            
+            
+            Optional<ButtonType> resultado = alerta.showAndWait();
+            
+            if (resultado.isPresent() && resultado.get() == ButtonType.OK) {    //Verifico que alla un resultado y que sea "OK"
+                administrador.eliminar(pf);
+            }
+            this.refrescarVista();
+        }        
+    }
+    
+    @FXML
+    void modificar(ActionEvent event) {
+         Vehiculo v = this.ListViewVehiculos.getSelectionModel().getSelectedItem();
+
+         if(v != null) {
+             this.AbrirFormulario(v);
+         }
+    }
+    
+    
+ //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////    
+    //Metodos especiales
+    
+    //Abir viewForm
+    private void AbrirFormulario(Vehiculo v ) {
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/viewFormulario.fxml"));
+    
+            Scene scene = new Scene(loader.load());
+            ViewFormularioController controlador = loader.getController();
+            Stage stage = new Stage();
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(scene);               
+            controlador.setVehiculo(v);      
+            stage.showAndWait();
+            Vehiculo resultado = controlador.getVehiculo();    
+   
+
+                if(resultado != null) {                        
+                    if(v == null){       
+                        if (!administrador.listarTodo().contains(resultado)){       
+                            this.administrador.agregar(resultado);
+                        }
+                    }
+                }
+                this.refrescarVista();
+        }
+        catch(IOException | PatenteRepetidaException e){
+            System.out.println("Error:" + e.getMessage());
+        }
+    }
+    
+    
+    
+    //Actualiza la vista
+    public void refrescarVista(){
+        this.ListViewVehiculos.getItems().clear();
+        this.ListViewVehiculos.getItems().addAll(administrador.listarTodo());
+    }
 }
--- GestorProductosFarmaceuticos/src/Controllers/ViewFormularioController.java
+++ GestorProductosFarmaceuticos/src/Controllers/ViewFormularioController.java
@@ -1,7 +1,11 @@
 package Controllers;
 
+import Enums.TipoCombustible;
+import Enums.TipoVehiculos;
+import Models.Vehiculo;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -10,11 +14,6 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 
 public class ViewFormularioController implements Initializable {
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     //Botones
     @FXML
     private Button btnAceptar;
@@ -23,11 +22,13 @@ public class ViewFormularioController implements Initializable {
     
     //Choice Boxes
     @FXML
-    private ChoiceBox<?> cbEstadoVehiculo;
+    private ChoiceBox<String> cbEstadoVehiculo;
     @FXML
-    private ChoiceBox<?> cbTipoCombustible;
+    private ChoiceBox<TipoCombustible> cbTipoCombustible;
     @FXML
-    private ChoiceBox<?> cbTipoVehiculo;
+    private ChoiceBox<TipoVehiculos> cbTipoVehiculo;
+    @FXML
+    private ChoiceBox<String> cbMarca;
     
     //Date picker
     @FXML
@@ -35,12 +36,86 @@ public class ViewFormularioController implements Initializable {
     
     //Labels
     @FXML
-    private Label lblPrimerAtributo;
-    @FXML
     private Label lblSegundoAtributo;
     @FXML
     private Label lblTiempoRenta;
+    
+    private Vehiculo v;
+    
+    
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        //Inicializo los ChoiceBox
+        this.cbEstadoVehiculo.getItems().addAll("Disponible", "En Uso", "En Matenimiento");
+        this.cbEstadoVehiculo.setValue("Disponible");
+        
+        this.cbTipoCombustible.getItems().addAll(TipoCombustible.DIESEL, TipoCombustible.NAFTA, TipoCombustible.ELECTRICO, TipoCombustible.HIBRIDO);
+        this.cbTipoCombustible.setValue(TipoCombustible.NAFTA);
+        
+        this.cbTipoVehiculo.getItems().addAll(TipoVehiculos.AUTO, TipoVehiculos.CAMIONETA, TipoVehiculos.MOTO);
+        this.cbTipoVehiculo.setValue(TipoVehiculos.AUTO);
+    }    
 
+    @FXML
+    void cambiadoTipo(ActionEvent event) {
+        //Dependiento del valor del choiseBox seteo un valor al label del datoExtra
+        switch(cbTipoVehiculo.getValue()){
+            
+            case AUTO -> {
+                // Ocultar campos de objetivo
+                this.cbMarca.getItems().addAll("FORD", "CHEVROLET",  "TOYOTA", "VOLKSWAGEN", "BMW",  "FIAT", "RENAULT", "NISSAN", "PEUGEOT");
+                this.cbMarca.setValue("FIAT");
+                lblSegundoAtributo.setText("Cantidad de Puertas: ");
+            }
+            
+            case CAMIONETA -> {
+                // Mostrar campos de objetivo
+                this.cbMarca.getItems().addAll("RENAULT", "NISSAN", "JEEP", "DODGE", "RAM");
+                this.cbMarca.setValue("RAM");
+                lblSegundoAtributo.setText("Capacidad de Carga: ");
+                
+            }
+            case MOTO -> {
+                // Ocultar campos de objetivo
+                this.cbMarca.getItems().addAll("HONDA",  "YAMAHA", "SUZUKI", "KAWASAKI", "BMW", "DUCATI", "MOTOMEL");
+                this.cbMarca.setValue("HONDA");
+                lblSegundoAtributo.setText("Cilindrada: ");
+            }
+        }
+        
+        public void setProducto(Vehiculo v){
+            this.producto = producto;
 
+            if (producto != null) {
+                // Campos comunes
+                txtNombre.setText(producto.getNombreComercial());
+                txtDosis.setText(producto.getDosis());
+                dpFechaVencimiento.setValue(producto.getFechaVencimiento());
 
+                if (producto instanceof Medicamento medicamento) {
+                    cbTipoProd.setValue("MEDICAMENTO");
+                    cbReceta.setSelected(medicamento.isRequerimientoRecetaMediica());
+                } else if (producto instanceof Suplemento suplemento) {
+                    cbTipoProd.setValue("SUPLEMENTO");
+                    txtObjetivo.setText(suplemento.getObjetivo());
+                }
+            } else {
+                // Si es nuevo, limpiar campos
+                txtNombre.clear();
+                txtDosis.clear();
+                dpFechaVencimiento.setValue(null);
+                txtObjetivo.clear();
+                cbReceta.setSelected(false);
+                cbTipoProd.setValue("MEDICAMENTO");
+            }
+
+        // Aplicar visibilidad según el tipo seleccionado
+        cambiadoTipo(null);
+    }
+    
+    public Vehiculo getProductoFarmaceutico(){
+        return this.v;
+    }
+       
 }
